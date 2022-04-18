f0a97231eb0ab93cb88d96a3b2849976401b6468
felitommasi <felipe.tommasi@gmail.com>
2022-04-17 23:50:59 -0300

    second commit

--- js/app.js
+++ js/app.js
@@ -28,15 +28,39 @@ function elegirEstacion(array){
     div.innerHTML += `
         <div class="btn-toolbar" role="toolbar">
             <div class="btn-group-vertical me-2">
-                <button type="button" class="btn btn-outline-secondary">
+                <button type="button" class="btn btn-outline-secondary mb-2" id="agregar${item.id}">
                     ${item.estacion}    
                 </button>
             </div>
         </div>
     `
     selecEstaciones.appendChild(div);
+
+        let btnAgregar = document.getElementById(`agregar${item.id}`);
+        btnAgregar.addEventListener('click', ()=>{
+            agregarEstacion(item.id);
+        })
     });
 }
 
 
+function agregarEstacion(id){
+    let agregarEstacionIda = arrayEstaciones.find(item => item.id == id);
+    estacionPartidaInput.value = JSON.stringify(agregarEstacionIda.estacion);
+
+}
+
+
+function agregarEstacionVuelta(){
+    let btnIdaVuelta = document.getElementById(selecPasajeDos);
+    btnIdaVuelta.addEventListener('click', ()=>{
+        agregarEstacion(item.id);
+    })
+}
+
+
 //logica simulador carga
+
+function cargarSaldo(){
+
+}
