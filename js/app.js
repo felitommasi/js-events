068222c594a518e59417d4e1bd82a81facc46cd6
felitommasi <felipe.tommasi@gmail.com>
2022-04-19 16:28:48 -0300

    events

--- js/app.js
+++ js/app.js
@@ -15,17 +15,15 @@ const btnCancelar = document.getElementById("btnCancelar");
 const btnIngresar = document.getElementById("btnIngresar");
 const btnCargar = document.getElementById("btnCargar");
 
-
-
 elegirEstacion(arrayEstaciones);
 
 //logica simulador pasaje
-function elegirEstacion(array){
-    array.forEach(item => {
-        let div = document.createElement('div');
-        div.classList.add('estacion');
+function elegirEstacion(array) {
+	array.forEach((item) => {
+		let div = document.createElement("div");
+		div.classList.add("estacion");
 
-    div.innerHTML += `
+		div.innerHTML += `
         <div class="btn-toolbar" role="toolbar">
             <div class="btn-group-vertical me-2">
                 <button type="button" class="btn btn-outline-secondary mb-2" id="agregar${item.id}">
@@ -33,34 +31,51 @@ function elegirEstacion(array){
                 </button>
             </div>
         </div>
-    `
-    selecEstaciones.appendChild(div);
+    `;
+		selecEstaciones.appendChild(div);
 
-        let btnAgregar = document.getElementById(`agregar${item.id}`);
-        btnAgregar.addEventListener('click', ()=>{
-            agregarEstacion(item.id);
-        })
-    });
+		let btnAgregar = document.getElementById(`agregar${item.id}`);
+		btnAgregar.addEventListener("click", () => {
+			agregarEstacion(item.id);
+			console.log(estacionPartidaInput.value);
+			console.log(valorPasajeInput.value);
+
+			if (estacionPartidaInput.value != "") {
+				let btnAgregar = document.getElementById(`agregar${item.id}`);
+                btnAgregar.addEventListener("click", () => {
+                    agregarEstacionDos(item.id);
+                })
+			}
+		});
+
+	});
 }
 
-
-function agregarEstacion(id){
-    let agregarEstacionIda = arrayEstaciones.find(item => item.id == id);
-    estacionPartidaInput.value = JSON.stringify(agregarEstacionIda.estacion);
-
+function agregarEstacion(id) {
+	let agregarEstacionIda = arrayEstaciones.find((item) => item.id == id);
+	estacionPartidaInput.value = JSON.stringify(agregarEstacionIda.estacion);
+	valorPasajeInput.value = JSON.stringify(agregarEstacionIda.precio);
 }
 
-
-function agregarEstacionVuelta(){
-    let btnIdaVuelta = document.getElementById(selecPasajeDos);
-    btnIdaVuelta.addEventListener('click', ()=>{
-        agregarEstacion(item.id);
-    })
+function agregarEstacionDos(id) {
+	let agregarEstacionDos = arrayEstaciones.find((item) => item.id == id);
+	estacionDestinoInput.value = JSON.stringify(agregarEstacionDos.estacion);
+	valorPasajeInput.value = JSON.stringify(agregarEstacionDos.precio);
 }
 
+/* function sumaPrecios(){
+    let sumaTotal = (val1, val2) =>{
+        return (JSON.stringify(agregarEstacionIda.precio)) + (JSON.stringify(agregarEstacionDos.precio));
+    }
+    console.log(sumaTotal);
+}
+sumaPrecios(); */
+
+/* function agregarEstacionVuelta(id){
+    let agregarEstacionVuelta = arrayEstaciones.find(item => item.id == id);
+    estacionDestinoInput.value = JSON.stringify(agregarEstacionIda.estacion);
+}  */
 
 //logica simulador carga
 
-function cargarSaldo(){
-
-}
+function cargarSaldo() {}
