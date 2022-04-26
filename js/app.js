bcc861702ee019ecc12f1fdb93e8c85ccf260010
felitommasi <felipe.tommasi@gmail.com>
2022-04-26 11:55:44 -0300

    array estaciones seccion

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-//get form elements
+//!get form elements
 window.addEventListener("load", () => {
 	const form = document.getElementById("form");
 	const tipoPasaje = document.getElementById("tipoPasaje");
@@ -9,23 +9,24 @@ window.addEventListener("load", () => {
 	const btnCancelar = document.getElementById("btnCancelar");
 	const inputs = document.getElementsByClassName("msjeError");
 
+	//!listado viajes realizados
+	const arrayViajesRealizados = [];
+
 	form.addEventListener("submit", (e) => {
 		e.preventDefault();
 		validaCampos();
 	});
 
-	//validacion form
+	//!validacion form
 	const validaCampos = () => {
-		//captura valores ingresados por el usuario
-		const tipoPasajeValor = tipoPasaje.value;
-		const partidaValor = partida.value;
-		const destinoValor = destino.value; 
 
 		//validar tipo pasaje
 		if (parseInt(tipoPasaje.value) === 0) {
 			validError();
 		} else {
 			validOk();
+			valorPasaje();
+			console.log(tipoPasaje.value);
 		}
 
 		//validar partida
@@ -33,6 +34,8 @@ window.addEventListener("load", () => {
 			validError();
 		} else {
 			validOk();
+			valorPasaje();
+			console.log(partida.value);
 		}
 
 		//validar destino
@@ -40,102 +43,41 @@ window.addEventListener("load", () => {
 			validError();
 		} else {
 			validOk();
+			valorPasaje();
+			console.log(destino.value);
 		}
 	};
 
 	//msje en caso de error
 	const validError = () => {
-	let formControl = document.querySelectorAll(".msjeError");
-	for(const el of formControl){
-		el.innerHTML = "Por favor completa este campo";
-	}
-	
-
-	console.log('error');
+		let formControl = document.querySelectorAll(".msjeError");
+		for (const el of formControl) {
+			el.innerHTML = "Por favor completa este campo";
+		}
+		console.log("error");
 	};
 
 	//msje en caso de correcto
 	const validOk = () => {
-		
-		//formControl.className = "form-control ok"
-
-		console.log('ok');
+		console.log("ok");
 	};
+
+
+	//!comparar input con array para conseguir valor pasaje
+	const valorPartida = parseInt(partida.value);
+	const valorDestino = parseInt(destino.value);
+	console.log(valorPartida);
+	console.log(valorDestino);
+
+	//console.log(arrayEstaciones[1].seccion)
+
+/* 	for(let id of arrayEstaciones){
+		console.log(id);
+	} */
+	for (let el = 0; el < arrayEstaciones.length; el++) {
+		const element = arrayEstaciones[el].seccion;
+		console.log(element);
+	} 
+
 });
 
-//listado viajes realizados
-const arrayViajesRealizados = [];
-
-//selector tipo de pasaje
-
-//aplicacion descuento en caso de seleccionar ida y vuelta
-
-/* //logica simulador pasaje
-elegirEstacion(arrayEstaciones);
-
-//seleccionar estaciones
-function elegirEstacion(array) {
-	array.forEach((item) => {
-		let div = document.createElement("div");
-		div.classList.add("estacion");
-
-		//mostrar estaciones en pantalla
-		div.innerHTML += `
-        <div class="btn-toolbar" role="toolbar">
-            <div class="btn-group-vertical me-2">
-                <button type="button" class="btn btn-outline-secondary mb-2" id="agregar${item.id}">
-                    ${item.estacion}    
-                </button>
-            </div>
-        </div>
-    `;
-		selecEstaciones.appendChild(div);
-
-		//evento para completar inputs
-		let btnAgregar = document.getElementById(`agregar${item.id}`);
-		btnAgregar.addEventListener("click", () => {
-			if (estacionPartidaInput.value === "") {
-				agregarEstacion(item.id);
-				console.log(item.id);
-				console.log(estacionPartidaInput.value);
-				console.log(valorPasajeInput.value);
-			}
-		});
-
-		let btnAgregarDos = document.getElementById(`agregar${item.id}`);
-		btnAgregarDos.addEventListener("click", () => {
-			if (estacionPartidaInput.value !== "") {
-				agregarEstacionDos(item.id);
-				console.log(item.id);
-				console.log(estacionDestinoInput.value);
-				console.log(estacionDestinoInput.value);
-			}
-		});
-	});
-}
-
-//obtener valores de estacion ida
-function agregarEstacion(id) {
-	let agregarEstacionIda = arrayEstaciones.find((item) => item.id == id);
-	selecEstacion = JSON.stringify(agregarEstacionIda.estacion);
-	let selecValor = JSON.stringify(agregarEstacionIda.precio);
-	estacionPartidaInput.value = selecEstacion;
-	valorPasajeInput.value = selecValor;
-}
-
-//obtener valores de estacion destino
-function agregarEstacionDos(id) {
-	let agregarEstacionVuelta = arrayEstaciones.find((item) => item.id == id);
-	selecEstacionDos = JSON.stringify(agregarEstacionVuelta.estacion);
-    let valorAnterior = document.getElementById("valorPasajeInput.value");
-    let selecValorSuma = (a,b) => {return a+b};
-	estacionDestinoInput.value = selecEstacionDos;
-	valorPasajeInput.value = selecValorSuma (valorAnterior, agregarEstacionVuelta.precio);
-}
- */
-
-//aplicar descuento "ida y vuelta" al valor total
-
-//logica simulador carga
-
-//    function () { }
--- js/estaciones.js
+++ js/estaciones.js
@@ -1,20 +1,23 @@
 const arrayEstaciones = [
-    {id: 1, seccion: 1, estacion: "PLAZA CONSTITUCION", precio: 50},
-    {id: 2, seccion: 1, estacion: "HIPOLITO YRIGOYEN", precio: 50},
-    {id: 3, seccion: 1, estacion: "D. SANTILLAN Y M. KOSTEKI", precio: 50},
-    {id: 4, seccion: 1, estacion: "GERLI", precio: 50},
-    {id: 5, seccion: 1, estacion: "LANUS", precio: 50},
-    {id: 6, seccion: 1, estacion: "ESCALADA", precio: 50},
-    {id: 7, seccion: 2, estacion: "BANFIELD", precio: 70},
-    {id: 8, seccion: 2, estacion: "LOMAS DE ZAMORA", precio: 70},
-    {id: 9, seccion: 2, estacion: "TEMPERLEY", precio: 70},
-    {id: 10, seccion: 2, estacion: "ADROGUE", precio: 70},
-    {id: 11, seccion: 2, estacion: "BURZACO", precio: 70},
-    {id: 12, seccion: 2, estacion: "LONGCHAMPS", precio: 70},
-    {id: 13, seccion: 3, estacion: "GLEW", precio: 90},
-    {id: 14, seccion: 3, estacion: "GURENICA", precio: 90},
-    {id: 15, seccion: 3, estacion: "A. KORN", precio: 90},
+    {id: 0, seccion: NaN, estacion: NaN},
+    {id: 1, seccion: 1, estacion: "PLAZA CONSTITUCION"},
+    {id: 2, seccion: 1, estacion: "HIPOLITO YRIGOYEN"},
+    {id: 3, seccion: 1, estacion: "D. SANTILLAN Y M. KOSTEKI"},
+    {id: 4, seccion: 1, estacion: "GERLI"},
+    {id: 5, seccion: 1, estacion: "LANUS"},
+    {id: 6, seccion: 1, estacion: "ESCALADA"},
+    {id: 7, seccion: 2, estacion: "BANFIELD"},
+    {id: 8, seccion: 2, estacion: "LOMAS DE ZAMORA"},
+    {id: 9, seccion: 2, estacion: "TEMPERLEY"},
+    {id: 10, seccion: 2, estacion: "ADROGUE"},
+    {id: 11, seccion: 2, estacion: "BURZACO"},
+    {id: 12, seccion: 2, estacion: "LONGCHAMPS"},
+    {id: 13, seccion: 3, estacion: "GLEW"},
+    {id: 14, seccion: 3, estacion: "GURENICA"},
+    {id: 15, seccion: 3, estacion: "A. KORN"},
 ];
 
-
+//valor seccion 1 = $50
+//valor seccion 2 = $70
+//valor seccion 3 = $90
 
